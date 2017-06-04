b6ff820429f9489e22cd27ce3a339f6d68340a6a
leonel-ai <leonelaguzman1@gmail.com>
2017-06-04 02:44:31 -0500

    contact files

---
+++ contact.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    // init the validator for contact form
+
+    $('#contact-form').validator();
+
+
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
+
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    // data = JSON object that contact.php returns
+
+                    // receive the type of the message: success x danger and apply it
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    // compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    
+                    // If messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
